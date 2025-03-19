ff7af6e435b92dd75c1a1742745d5bf78726d96d
hhhi21g <xuyuetong2025@163.com>
2025-03-19 14:45:42 +0800

    Update BP.py

--- exer_02/BP.py
+++ exer_02/BP.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder
 
 
 def load_data(fileName):
@@ -17,6 +18,12 @@ def init_param(num_inputs, num_hiddens, num_outputs):
     return w1, b1, w2, b2
 
 
+def encode_y(y):
+    encoder = OneHotEncoder()
+    y_onehot = encoder.fit_transform(y)
+    return y_onehot
+
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
@@ -33,10 +40,25 @@ def forward(X, w1, b1, w2, b2):
     A2 = sigmoid(Z2)
     return Z1, A1, Z2, A2
 
+def backward(X,y,Z1,A1,A2,W2):
+    
+
+
+def train(X, y, num_hiddens, lr=0.1, epochs=10):
+    num_inputs = X.shape[1]
+    num_outputs = y.shape[1]
+    w1, b1, w2, b2 = init_param()
+
+    for epoch in range(epochs):
+        Z1,A1,Z2,A2 = forward(X,w1,b1,w2,b2)
+
 
 if __name__ == "__main__":
     train_features, train_labels = load_data("Iris-train.txt")
     test_features, test_labels = load_data("Iris-test.txt")
+
+    # 返回每个样本属于每个种类的概率，这里使用独热编码处理
+    train_labels_onehot = encode_y(train_labels)
 
     num_inputs, num_outputs, num_hiddens = train_features.shape[1], 3, 10
     w1, b1, w2, b2 = init_param(num_inputs, num_hiddens, num_outputs)
